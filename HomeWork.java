7591f7993a9534e8972f12bb00b16c76c55ea932
maxbes74 <99111042+maxbes74@users.noreply.github.com>
2022-08-02 15:28:11 +0500

    Ввод от пользователя в цикле Массив вариантов ответа

--- HomeWork.java
+++ HomeWork.java
@@ -6,34 +6,41 @@ public class HomeWork {
         int userCorrectAnswer = 0;
         int userNotCorrectAnswer = 0;
 
-        String[][] questions = {
-                {"В файл с каким расширением компилируется java-файл?", "1. cs", "2. java", "3. class", "4. exe"},
-                {"С помощью какой команды git можно получить полную копию удаленного репозитория?", "1. commit", "2. push", "3. clone", "4. copy"},
-                {"Какой применяется цикл, когда не известно количество итераций?", "1. while", "2. for", "3. loop",}
+        String[] questions = {
+                "В файл с каким расширением компилируется java-файл?",
+                "С помощью какой команды git можно получить полную копию удаленного репозитория?",
+                "Какой применяется цикл, когда не известно количество итераций?"
         };
-
+        String[][] answerOptions = {
+                {"1. cs", "2. java", "3. class", "4. exe"},
+                {"1. commit", "2. push", "3. clone", "4. copy"},
+                {"1. while", "2. for", "3. loop",}
+        };
         int[] correctAnswers = {2, 3, 1};
-        int k = 0;
-        for (int i = 0; i < questions.length; i++) {
-            for (int j = 0; j < questions[i].length; j++) {
-                System.out.println(questions[i][j]);
+        int correctAnswerNumber = 0;
+        int questionNumber = 0;
+
+        for (int j = 0; j < answerOptions.length; j++) {
+            System.out.println(questions[questionNumber]);
+            for (int k = 0; k < answerOptions[j].length; k++) {
+                System.out.println(answerOptions[j][k]);
             }
-            if (cs.hasNextInt()) {
-                int userVariants = cs.nextInt();
-                if (userVariants == correctAnswers[k]) {
-                    System.out.println("Правильно");
-                    userCorrectAnswer++;
-                } else {
-                    System.out.println("Неправильно");
-                    userNotCorrectAnswer++;
-                }
+            System.out.println("Ваш ответ: ");
+            while (!cs.hasNextInt()) {
+                System.out.println("Введите номер ответа");
+                cs.next();
+            }
+            int userVariants = cs.nextInt();
+            if (userVariants == correctAnswers[correctAnswerNumber]) {
+                System.out.println("Правильно");
+                userCorrectAnswer++;
             } else {
                 System.out.println("Неправильно");
                 userNotCorrectAnswer++;
-                cs.next();
             }
-            k++;
+            correctAnswerNumber++;
         }
+        questionNumber++;
         System.out.println("Результат: правильно " + userCorrectAnswer + ", неправильно " + userNotCorrectAnswer);
     }
 }
