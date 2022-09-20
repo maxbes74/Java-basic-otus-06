6be2b4706e1422c59090de98afb7d9e05367b099
maxbes74 <99111042+maxbes74@users.noreply.github.com>
2022-09-20 16:37:21 +0500

    HW - 6 v5

--- Bank.java
+++ Bank.java
@@ -1,23 +1,66 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Bank {
     public static void main(String[] args) {
 
         Client ivanov = new Client("Иванов И.И.", 56);
-        Account accountIvanov = new Account();
-        Account accountIvanov2 = new Account();
-        accountIvanov.setNumberOfCoins(100);
-        accountIvanov2.setNumberOfCoins(30);
+        Client sidorov = new Client("Сидоров А.П.", 25);
+        Client petrov = new Client("Петров Г.М.", 30);
 
-        ivanov.addAccount(accountIvanov);
+        Account accountIvanov1 = new Account(1);
+        Account accountIvanov2 = new Account(2);
+
+        Account accountSidorov1 = new Account(3);
+        Account accountSidorov2 = new Account(4);
+        Account accountSidorov3 = new Account(5);
+
+        Account accountPetrov1 = new Account(6);
+
+
+//        accountIvanov1.setNumberOfCoins(100);
+//        accountIvanov2.setNumberOfCoins(30);
+//
+//        accountSidorov1.setNumberOfCoins(10);
+//        accountSidorov2.setNumberOfCoins(1000);
+//        accountSidorov3.setNumberOfCoins(5);
+//
+//        accountPetrov1.setNumberOfCoins(58);
+
+
+// добавление счета клиенту
+        ivanov.addAccount(accountIvanov1);
         ivanov.addAccount(accountIvanov2);
-        System.out.println(ivanov);
+
+        petrov.addAccount(accountPetrov1);
+
+        sidorov.addAccount(accountSidorov1);
+        sidorov.addAccount(accountSidorov2);
+        sidorov.addAccount(accountSidorov3);
+
+
+//полная информация о клиенте
+
+//        System.out.println(ivanov);
+//        System.out.println(petrov);
+//        System.out.println(sidorov);
+
         Map<Client, List<Account>> clients = new HashMap<>();
-        clients.put(ivanov,ivanov.getListAccount());
-        System.out.println(clients.get(ivanov));
+        clients.put(ivanov, ivanov.getListAccount());
+        clients.put(petrov, petrov.getListAccount());
+        clients.put(sidorov, sidorov.getListAccount());
+
+//        for ( Map.Entry <Client, List<Account>> entry : clients.entrySet()) {
+//            System.out.println(entry.getKey());
+//
+//        }
+
+
+        // информация о счетах и количестве монет по клиенту.
+
+//        System.out.println(clients.get(ivanov));
+//        System.out.println(clients.get(petrov));
+//        System.out.println(clients.get(sidorov));
     }
+
 
 }
--- Client.java
+++ Client.java
@@ -3,28 +3,31 @@ import java.util.*;
 public class Client {
     private final String fullName;
     private final int age;
-    private List <Account> listAccount;
+    private final List<Account> listAccount;
 
     public Client(String fullName, int age) {
         this.fullName = fullName;
         this.age = age;
         this.listAccount = new ArrayList<>();
     }
-    public void addAccount (Account account){
+
+    public void addAccount(Account account) {
         this.listAccount.add(account);
     }
 
     @Override
     public String toString() {
-        return "Client{" +
-                "fullName='" + fullName + '\'' +
-                ", age=" + age +
-                ", listAccount=" + listAccount +
+        return "Клиент: " +
+                "Ф.И.О. = '" + fullName + '\'' +
+                ", возраст = " + age +
+                ", Счета: " + listAccount +
                 '}';
     }
 
     public List<Account> getListAccount() {
         return listAccount;
     }
+
 }
+
 
