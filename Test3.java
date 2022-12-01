c5167576531675a36b82a4de12eba2f91af01834
maxbes74 <99111042+maxbes74@users.noreply.github.com>
2022-12-01 14:48:40 +0500

    HW - 7 final

--- Test3.java
+++ Test3.java
@@ -1,11 +1,17 @@
+import java.util.Arrays;
+
 public class Test3 {
 
     public void printArr() {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         try {
-            System.out.println(arr[10]);
+            System.out.println("Вывод  элемента n = " + arr[10]);
         } catch (ArrayIndexOutOfBoundsException e) {
-            System.exit(777);
+            e.printStackTrace();
+        } finally {
+            System.out.println("Длина массива = " + arr.length);
+            System.out.println("Массив = " + Arrays.toString(arr));
+
         }
 
     }
