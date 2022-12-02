a106273fcfc017e884e0b8cd37d7ec832ebc5b4e
maxbes74 <99111042+maxbes74@users.noreply.github.com>
2022-12-02 11:26:18 +0500

    HW - 6 final

--- Bank.java
+++ Bank.java
@@ -1,7 +1,9 @@
 import java.util.*;
 
 public class Bank {
+
     private static final Map<Client, List<Account>> clients = new HashMap<>();
+    private static final Map<Account, Client> accounts = new HashMap<>();
 
     public static void main(String[] args) {
 
@@ -10,56 +12,66 @@ public class Bank {
         Client petrov = new Client("Петров Г.М.", 30);
 
         Account account1 = new Account(1);
-        Account account2 = new Account(2);
-        List<Account> listAccount1 = new ArrayList<>();
-        listAccount1.add(account1);
-        listAccount1.add(account2);
+        Account account2 = new Account(2, 30);
+        List<Account> ivanovAccounts = List.of(account1, account2);
 
-        Account account3 = new Account(3);
-        Account account4 = new Account(4);
-        Account account5 = new Account(5);
-        List<Account> listAccount2 = new ArrayList<>();
-        listAccount2.add(account3);
-        listAccount2.add(account4);
-        listAccount2.add(account5);
 
-        Account account6 = new Account(6);
-        List<Account> listAccount3 = new ArrayList<>();
-        listAccount3.add(account6);
+        Account account3 = new Account(3, 10);
+        Account account4 = new Account(4, 1000);
+        Account account5 = new Account(5, 5);
+        List<Account> sidorovAccounts = List.of(account3, account4, account5);
 
-//добавление монет
-        account1.setNumberOfCoins(100);
-        account2.setNumberOfCoins(30);
 
-        account3.setNumberOfCoins(10);
-        account4.setNumberOfCoins(1000);
-        account5.setNumberOfCoins(5);
+        Account account6 = new Account(6, 58);
+        List<Account> petrovAccounts = List.of(account6);
 
-        account6.setNumberOfCoins(58);
 
-//        Map<Client, List<Account>> clients = new HashMap<>();
-        clients.put(ivanov, listAccount1);
-        clients.put(petrov, listAccount3);
-        clients.put(sidorov, listAccount2);
+        //добавление монет
+        //account1.setNumberOfCoins(100);
+        //account2.setNumberOfCoins(30);
+
+        //account3.setNumberOfCoins(10);
+        //account4.setNumberOfCoins(1000);
+        //account5.setNumberOfCoins(5);
+
+        //account6.setNumberOfCoins(58);
+
+        putClientAndAccounts(ivanov, ivanovAccounts);
+        putClientAndAccounts(sidorov, sidorovAccounts);
+        putClientAndAccounts(petrov, petrovAccounts);
 
         //полная информация о клиенте
-        infoClient(ivanov);
+//        infoClient(ivanov);
         // информация о счетах и количестве монет по клиенту.
-        getAccounts(sidorov);
-        // информация о кленте по счету.
-        findClient(account6);
+//        getAccounts(sidorov);
+        // информация о клиенте по счету.
+        getClient(account5);
+    }
+
+    public static void putClientAndAccounts(Client client, List<Account> accounts) {
+        for (Account account: accounts) {
+            putClientAndAccount(client, account);
+        }
+    }
+
+    private static void putClientAndAccount(Client client, Account newAccount) {
+        // положим в первую
+        List<Account> oldAccountList = new ArrayList<>();
+        if (clients.get(client) != null) {
+            oldAccountList = clients.get(client);
+        }
+        oldAccountList.add(newAccount);
+        clients.put(client, oldAccountList);
+        // положим во вторую
+        accounts.put(newAccount, client);
     }
 
     public static void getAccounts(Client client) {
-
         System.out.println(clients.get(client));
     }
 
-    public static void findClient(Account account) {
-        for (Map.Entry<Client, List<Account>> entry : clients.entrySet()) {
-            if (entry.getValue().contains(account))
-                System.out.println(entry.getKey());
-        }
+    public static void getClient(Account account) {
+        System.out.println(accounts.get(account));
     }
 
     public static void infoClient(Client client) {
