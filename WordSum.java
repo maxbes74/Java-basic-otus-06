e0de7d97c150618ad4f770cdfc079adf721842c9
maxbes74 <99111042+maxbes74@users.noreply.github.com>
2023-02-15 11:15:41 +0500

    HW - 12 Исправления кода + тесты

--- WordSum.java
+++ WordSum.java
@@ -1,7 +1,9 @@
 public class WordSum {
     public static String wordSum(int n) {
 
-        if (n % 10 == 1 && n != 11)
+        if (n % 100 >= 5 && n % 100 <= 20) {
+            return ("рублей");
+        } else if (n % 10 == 1)
             return ("рубль");
         else if (n % 10 >= 2 && n % 10 <= 4 && n / 10 != 1) {
             return ("рубля");
