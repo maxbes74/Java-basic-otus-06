b8a3a54862b206743cc5450b631ce96fd0b4ca94
maxbes74 <99111042+maxbes74@users.noreply.github.com>
2023-02-16 15:35:21 +0500

    HW - 12 Выбор валюты, юнит тесты разные валюты

--- CurrencyTest.java
+++ CurrencyTest.java
@@ -13,12 +13,12 @@ class CurrencyTest {
     @ParameterizedTest
     @ValueSource(ints = {23, 102, 1102})
     void EndOfTestingForCurrency2(int number) {
-        Assertions.assertEquals("рубля", Currency.RUB.wordSum(number));
+        Assertions.assertEquals("юаня", Currency.CNY.wordSum(number));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {5, 7, 10, 11, 12, 17, 20, 27, 100, 105, 110, 111, 112, 115, 120, 125, 200, 1000, 1100, 1111})
     void EndOfTestingForCurrency3(int number) {
-        Assertions.assertEquals("рублей", Currency.RUB.wordSum(number));
+        Assertions.assertEquals("долларов", Currency.USD.wordSum(number));
     }
 }
